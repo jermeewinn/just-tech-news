9e2dd0c39cc1754d6fd972cd2ac1aecf6f4c2075
Jeremy Huynh <jeahuynh18@gmail.com>
2021-12-16 20:50:50 -0800

    created routes for user and pw protection

---
+++ routes/api/index.js
@@ -0,0 +1,6 @@
+//This index.js will serve as a means to collect all of the API routes and package them up for us just like models/index.js.
+const router = require('express').Router();
+const userRoutes = require('./user-routes.js');
+router.use('/users', userRoutes);
+
+module.exports = router;
